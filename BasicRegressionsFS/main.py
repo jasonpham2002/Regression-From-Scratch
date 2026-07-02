fd1a5ffd6e345350604f41d36ae2c9c81845d3d4
Jason Pham <phamtuong20feb@gmail.com>
2026-07-02 09:45:30 +0700

    Restructured project files for better organization and updated metadata execution timestamps.

--- BasicRegressionsFS/main.py
+++ BasicRegressionsFS/main.py
@@ -44,7 +44,7 @@ class LinearRegressionFS:
         #initializae w
         self.w = np.zeros((n,1), dtype = np.float64)
 
-        # convert y from pandas dataframe to a numpy array
+        # convert y from pandas dataframe to a numpy arra   y
 
 
         self.gradient_descent(self.compute_gradient, X, y, self.lr, self.n_iters, self.compute_cost)
